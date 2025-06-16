34e81c9831764d4e37d069ef24866d5158986f1c
Jack-D-Sparrow <jack.d@jove.com>
2025-06-17 00:18:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -26,3 +26,8 @@ def extract():
         text = page.get_text().strip()
         result.append({'page': i + 1, 'text': text})
     return jsonify(result)
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
